66b0e9bec48a08b704b41c786abc350a4edf4a1c
zaid0080 <70195293+zaid0080@users.noreply.github.com>
2020-11-18 11:54:33 +0530

    Adding fonts and box-sizing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import { FaTwitter } from 'react-icons/fa';
 
-//const data  = require('./country.json');
-
-// function logger(e){
-//     console.log(e);
-// }
-
 function Header() {
     return (
         <div id='container'>
-            <h3><span><FaTwitter /></span>The latest twitter trends in one place</h3>]
-      
-            {/* <select className='drop-down' onClick={e => {
-                return console.log(e.target.value)
-            }}>
-                {data.map(d => {
-                    return (
-                        <option className='drop-down-items' key={d.woeid} value={d.woeid}>{d.name}</option>
-                    )
-                })}
-            </select> */}
+            <h3><span><FaTwitter /></span>The latest twitter trends in one place</h3>
         </div>
     )
 }
