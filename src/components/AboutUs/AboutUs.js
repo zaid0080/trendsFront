5a5b3cb5d9dc7d11b26fd06e64aa21970d866958
ibnjunaid <osamabinjunaid36@gmail.com>
2021-05-07 22:11:13 +0530

    fixed src typo in About.js

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -6,7 +6,7 @@ function AboutUs() {
   return (
     <div className='about-page'>
       <h1 className='title'>ABOUT alldaytrends.</h1>
-      <img scr={Trends} alt='About' className='about-img' />
+      <img src={Trends} alt='About' className='about-img' />
       <p className='tagLines'>
         When you are looking for ways to increase your social media exposure,
         you need to know and keep up with topics that are trending all around
