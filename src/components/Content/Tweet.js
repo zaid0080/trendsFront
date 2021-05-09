341761acdbab1e5e62258f18f90d61a43947718f
Zaid Akhter <zaidakhter0080@gmail.com>
2021-05-09 15:44:50 +0530

    tweet volume change

--- src/components/Content/Tweet.js
+++ src/components/Content/Tweet.js
@@ -3,9 +3,17 @@ import { GlobalContext } from "../../global";
 import { findDuration } from './Time';
 import './tweet.css';
 
+function changetoK(x){
+    if(x>1000){
+    return Math.floor(x / 1000)+'k';
+    }
+    return x;
+    }
+
 function Tweet() {
     const [, , data,] = useContext(GlobalContext);
     console.log(data);
+    
     return (
         <div className='trends-cont'>
             {data.map((d, index) => {
@@ -16,11 +24,11 @@ function Tweet() {
                         <ol>
                             {d.trends.map((t) => {
                                 if(t.tweet_volume > 0) {
-                                    return <li key={t.index}> {t.name} <small>{t.tweet_volume}</small> </li>
+                                    return <li className='tweet-names' key={t.index}> {t.name} <span className='tweet-volume'>{changetoK(t.tweet_volume)}</span> </li>
                                 }
                                 return  (
                                     <>
-                                        <li key={t.index}> {t.name} </li>
+                                        <li className='tweet-names' key={t.index}> {t.name} </li>
                                         
                                     </>
                                 )
