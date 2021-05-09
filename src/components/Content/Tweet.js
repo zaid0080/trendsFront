0ac643dcd5b65c2e46e4f581ca2cc6ffbb2ce5e6
Zaid Akhter <zaidakhter0080@gmail.com>
2021-05-09 13:49:08 +0530

    Website view changes

--- src/components/Content/Tweet.js
+++ src/components/Content/Tweet.js
@@ -15,7 +15,15 @@ function Tweet() {
                         <h4 data-time={d._id}>{findDuration(d._id)}</h4>
                         <ol>
                             {d.trends.map((t) => {
-                                return <li key={t.index}> {t.name} </li>
+                                if(t.tweet_volume > 0) {
+                                    return <li key={t.index}> {t.name} <small>{t.tweet_volume}</small> </li>
+                                }
+                                return  (
+                                    <>
+                                        <li key={t.index}> {t.name} </li>
+                                        
+                                    </>
+                                )
                             })}
                         </ol>
                         </div>
