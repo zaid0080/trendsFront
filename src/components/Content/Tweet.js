383ac5d4c6c71a138ac808a7d16b13ebddd890c7
Zaid Akhter <zaidakhter0080@gmail.com>
2021-05-09 17:40:37 +0530

    numbering

--- src/components/Content/Tweet.js
+++ src/components/Content/Tweet.js
@@ -21,10 +21,10 @@ function Tweet() {
                     <>  
                         <div className='card'>
                         <h4 data-time={d._id}>{findDuration(d._id)}</h4>
-                        <ol>
+                        <ol className='tweets'>
                             {d.trends.map((t) => {
                                 if(t.tweet_volume > 0) {
-                                    return <li className='tweet-names' key={t.index}> {t.name} <span className='tweet-volume'>{changetoK(t.tweet_volume)}</span> </li>
+                                    return <li className='tweet-names' key={t.index}> <span className='index'>{t.index}</span> {t.name} <span id='content' className='tweet-volume'>{changetoK(t.tweet_volume)}</span> </li>
                                 }
                                 return  (
                                     <>
