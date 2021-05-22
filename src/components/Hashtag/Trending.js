a71ea290f578068e384a99c67a4c5d48215ab017
Zaid Akhter <zaidakhter0080@gmail.com>
2021-05-22 13:16:25 +0530

    sidebar fixed

--- src/components/Hashtag/Trending.js
+++ src/components/Hashtag/Trending.js
@@ -1,31 +1,42 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../../global'
-import {fetchAndSetData} from '../Content/Content';
-
+import React, { useContext } from "react";
+import { GlobalContext } from "../../global";
+import { fetchAndSetData } from "../Content/Content";
+import { Link } from "react-router-dom";
 
 function Trending() {
-    const {city,country,setData, setTime} = useContext(GlobalContext);
-    let sessionData = window.sessionStorage.getItem('data');
-    if(sessionData === null){
-        const query = city === undefined ? country : city;
-        fetchAndSetData(query,setData,setTime);
-        sessionData = window.sessionStorage.getItem('data');
-    }
-    const d = JSON.parse(sessionData);
-    console.log(d[0]);
-    if(d.length){
-        return (
-            <>
-                <p className='tt-tag'>Trending Now</p>      
-                <ul className='tt-trends'>
-                   {d[0].trends.map(d => <li key={d.index} >{d.name}</li>)}
-                </ul>      
-            </>
-        )
-    } else {
-        return null
-    }
-    
+  const { city, country, setData, setTime } = useContext(GlobalContext);
+  let sessionData = window.sessionStorage.getItem("data");
+  if (sessionData === null) {
+    const query = city === undefined ? country : city;
+    fetchAndSetData(query, setData, setTime);
+    sessionData = window.sessionStorage.getItem("data");
+  }
+  const d = JSON.parse(sessionData);
+  // console.log(d[0]);
+  if (d) {
+    return (
+      <>
+        <p className="tt-tag">Trending Now</p>
+        <ul className="tt-trends">
+          {d[0].trends
+            .filter((d, i) => i < 10)
+            .map((d) => (
+              <li key={d.index} className={`tweet-names-${d.index}`}>
+                <Link
+                  to={`/${country}${
+                    city === undefined ? "" : "/" + city
+                  }/trend/${d.name.replace(/#/g, "_")}`}
+                >
+                  {d.name}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
 
-export default Trending
+export default Trending;
