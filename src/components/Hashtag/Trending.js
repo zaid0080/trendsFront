cffea03f37c36836de90c33d54587468bb09acc5
ibnjunaid <osamabinjunaid36@gmail.com>
2021-05-26 19:48:43 +0530

    console.logged TypeError

--- src/components/Hashtag/Trending.js
+++ src/components/Hashtag/Trending.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function Trending() {
   const { city, country, setData, setTime } = useContext(GlobalContext);
   const [placeError, setPlaceError] = useState(null);
+  console.log(placeError);
   
   let sessionData = window.sessionStorage.getItem("data");
   if (sessionData === null) {
