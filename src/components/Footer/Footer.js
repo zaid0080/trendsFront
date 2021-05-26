24bc578faf9a241a84ac596c7a09a3fbd60b8692
Zaid Akhter <zaidakhter0080@gmail.com>
2021-05-26 17:57:37 +0530

    Donee...

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
-// import ADT from "../NavBar/logoTag.png";
-import Link from 'next/link'
+import ADT from "../NavBar/logoTag.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div id="foots-container">
-      {/* <div className="logo-tag">
+      <div className="logo-tag">
         <Link to="/">
           <img src={ADT} alt="Logo" height="35" width="200" />
         </Link>
-      </div> */}
+      </div>
       <div className="footer-links">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+        <Link to="/">Home</Link>
+        <Link to="/aboutus">About</Link>
         <a
           href="https://twitter.com/login?lang=en-gb"
           target="_blank"
