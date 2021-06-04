d2ea11c3ed31c9ac288e176bd1035ea4a30bbaa2
Zaid Akhter <zaidakhter0080@gmail.com>
2021-06-04 14:46:53 +0530

    follow us icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ADT from "../NavBar/logoTag.png";
 import { Link } from "react-router-dom";
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaTwitterSquare,
+  FaRedditSquare,
+} from "react-icons/fa";
 
 function Footer() {
   return (
@@ -13,13 +19,43 @@ function Footer() {
       <div className="footer-links">
         <Link to="/">Home</Link>
         <Link to="/aboutus">About</Link>
-        <Link to='/faq'>FAQ</Link>
+        <Link to="/faq">FAQ</Link>
         <a
           href="https://twitter.com/login?lang=en-gb"
           target="_blank"
           rel="noreferrer"
         >
           Login
+        </a>
+      </div>
+      <div className="icons-container">
+        <a
+          href="https://www.facebook.com/alldaytrends/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookSquare className="icons" />
+        </a>
+        <a
+          href="https://www.instagram.com/alldaytrends1/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram className="icons" />
+        </a>
+        <a
+          href="https://twitter.com/alldaytrends1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitterSquare className="icons" />
+        </a>
+        <a
+          href="https://www.reddit.com/user/alldaytrends"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaRedditSquare className="icons" />
         </a>
       </div>
     </div>
