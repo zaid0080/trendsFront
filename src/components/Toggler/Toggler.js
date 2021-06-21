0b99102870f47cf23465abc3b2a4ef65ae8f0bc6
Zaid Akhter <zaidakhter0080@gmail.com>
2021-06-21 23:46:08 +0530

    dark mode finally done

--- src/components/Toggler/Toggler.js
+++ src/components/Toggler/Toggler.js
@@ -1,16 +1,13 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from "../../global";
 import './toggler.css'
-// import { HiOutlineLightBulb } from 'react-icons/hi'
-// import { IoMdCloudyNight  } from 'react-icons/io'
 
 function Toggler() {
   const {darkMode, setDarkMode} = useContext(GlobalContext);
     return (
         <div className='toggle-cont'>
             <div className='toggle-icon'>
-                    <input type='checkbox' id='toggle' onClick={() => setDarkMode(!darkMode)} checked={darkMode ? true : false} />
-                    <label for='toggle' className='toggler'></label>
+                    <input type='checkbox' id='toggle' onClick={() => setDarkMode(!darkMode)} defaultChecked={darkMode ? true : false} />
             </div>
         </div>
     )
--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
-import { useState, createContext, useEffect } from "react";
+import { useState, createContext } from "react";
 
 export const GlobalContext = createContext();
 
@@ -8,17 +8,7 @@ export const GlobalProvider =  props => {
     const [data,setData] = useState([]);
     const [selectedTime,setSelectedTime] = useState();
     const [selectedData,setSelectedData] = useState();
-    const [darkMode, setDarkMode] = useState(false);
-
-    
-    
-    useEffect(() => {
-        const darkOs = window.matchMedia(
-            "(prefers-color-scheme: dark)"
-          ).matches
-          darkOs ? setDarkMode(true) : setDarkMode(false)
-    }, [])
-
+    const [darkMode, setDarkMode] = useState(true);
     
     return (
         <GlobalContext.Provider value={{country,setCountry,city,setCity,data,setData,selectedTime,setSelectedTime,selectedData,setSelectedData,darkMode,setDarkMode}}>
