164a82460bb434c4c41af69a3194498cc9dd4263
Zaid Akhter <zaidakhter0080@gmail.com>
2021-10-23 12:42:50 +0530

    Image Resize

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from "../../global";
-import Social1 from './Social1.png';
+import Social from './Social.png';
 
 function Header() {
     const {country, city, darkMode} = useContext(GlobalContext);
@@ -10,7 +10,7 @@ function Header() {
             <h1 className={`page-title ${darkMode ? 'dark-text' : 'light-text' }`}>Top Twitter Trends - {city === undefined ? country : city}</h1>
         <div id='header-container'>
             <h2 className={`tag-line ${darkMode ? 'dark-text' : 'light-text'}`}>Know<span>What's</span><span>Trending?</span></h2>
-            <img src={Social1} width='726' height='494' alt='Social' className='head-image' />
+            <img src={Social} width='726' height='494' alt='Social' className='head-image' />
         </div>
         </>
     )
